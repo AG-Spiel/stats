f1d7574e95dedfaa00ccc5e716f6293a097b5896
UsingPython <42588836+UsingPython@users.noreply.github.com>
2021-02-21 22:06:48 +0100

    feat(app): Add navbar to start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { NavbarModule } from './shared/navbar/navbar.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { MaterialModule } from './material/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    NavbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
