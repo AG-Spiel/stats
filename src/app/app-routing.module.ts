cbb067e1f174e6f3e81e76231ffeb4c2f4d8ca4b
UsingPython <42588836+UsingPython@users.noreply.github.com>
2021-03-01 23:58:48 +0100

    feat(routing): Enable lazy loading and hash mode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/landing/landing.module').then((m) => m.LandingModule),
+  },
+  {
+    path: 'markt',
+    loadChildren: () =>
+      import('./features/market/market.module').then((m) => m.MarketModule),
+  },
+  {
+    path: 'wkn',
+    loadChildren: () =>
+      import('./features/shares/shares.module').then((m) => m.SharesModule),
+  },
+  {
+    path: 'index',
+    loadChildren: () =>
+      import('./features/indizes/indizes.module').then((m) => m.IndizesModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
