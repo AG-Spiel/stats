fbc4476737a8a983d2b5a1b77bfef33a2c15de54
UsingPython <42588836+UsingPython@users.noreply.github.com>
2021-03-08 18:09:12 +0100

    feat(market): Implement market line charts

--- src/app/features/market/pages/market-board/market-board.component.ts
+++ src/app/features/market/pages/market-board/market-board.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { URLS } from 'src/app/core/constants/urls.constants';
+import {
+  COVER_CHART_SERIES,
+  DEPOT_CHART_SERIES,
+  MARKET_CHART_SERIES,
+  ORDER_CHART_SERIES,
+  PREM_CHART_SERIES,
+} from '../../models/charts';
 
 @Component({
   selector: 'app-market-board',
@@ -6,6 +14,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./market-board.component.scss'],
 })
 export class MarketBoardComponent implements OnInit {
+  marketChartUrl = URLS.AGS_STATS.MARKET.MARKET;
+  marketChartSeries = MARKET_CHART_SERIES;
+  orderChartUrl = URLS.AGS_STATS.MARKET.ORDER;
+  orderChartSeries = ORDER_CHART_SERIES;
+  depotChartUrl = URLS.AGS_STATS.MARKET.DEPOT;
+  depotChartSeries = DEPOT_CHART_SERIES;
+  coverChartUrl = URLS.AGS_STATS.MARKET.COVER;
+  coverChartSeries = COVER_CHART_SERIES;
+  premChartUrl = URLS.AGS_STATS.MARKET.PREMIUM;
+  premChartSeries = PREM_CHART_SERIES;
+
   constructor() {}
 
   ngOnInit(): void {}
