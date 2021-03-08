a5a932a287478d1d91aface8a7760b6808e3faca
UsingPython <42588836+UsingPython@users.noreply.github.com>
2021-03-08 18:06:56 +0100

    feat(charts-theme): Setup charts theme changer

--- src/app/core/services/theme.service.spec.ts
+++ src/app/core/services/theme.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { DEFAULT_THEME } from '../constants/themes.constants';
+import { LocalStorageService } from './local-storage.service';
 import { StyleManagerService } from './style-manager.service';
 
 import { ThemeService } from './theme.service';
@@ -7,17 +9,23 @@ describe('ThemeService', () => {
   let service: ThemeService;
 
   let mockStyleManagerService: jasmine.SpyObj<StyleManagerService>;
+  let mockStorageService: jasmine.SpyObj<LocalStorageService>;
 
   beforeEach(() => {
     mockStyleManagerService = jasmine.createSpyObj('StyleManagerService', [
       'setStyle',
     ]);
+    mockStorageService = jasmine.createSpyObj('StorageService', ['getItem']);
 
     TestBed.configureTestingModule({
       providers: [
         {
           provide: StyleManagerService,
           useValue: mockStyleManagerService,
+        },
+        {
+          provide: LocalStorageService,
+          useValue: mockStorageService,
         },
       ],
     });
@@ -45,5 +53,58 @@ describe('ThemeService', () => {
       'theme',
       `assets/${themeName}.css`
     );
+  });
+
+  it('should set default themes on chart and website', () => {
+    const setChartSpy = spyOn(service, 'setChartTheme');
+
+    service.setDefaultTheme();
+
+    expect(mockStyleManagerService.setStyle).toHaveBeenCalledWith(
+      'theme',
+      `assets/${DEFAULT_THEME}.css`
+    );
+    expect(setChartSpy).toHaveBeenCalledWith(DEFAULT_THEME);
+  });
+
+  it('should get theme from storage', () => {
+    const resTheme = 'test';
+    mockStorageService.getItem.and.returnValue(resTheme);
+
+    const res = service.getTheme();
+
+    expect(res).toBe(resTheme);
+  });
+
+  it('should get theme object from storage', () => {
+    const testTheme = 'deeppurple-amber';
+    const getThemeSpy = spyOn(service, 'getTheme').and.returnValue(testTheme);
+
+    const res = service.getThemeObj();
+
+    expect(getThemeSpy).toHaveBeenCalled();
+    expect(res?.value).toBe(testTheme);
+  });
+
+  it('should get undefined theme if local storage empty', () => {
+    const testTheme = null;
+    const getThemeSpy = spyOn(service, 'getTheme').and.returnValue(testTheme);
+
+    const res = service.getThemeObj();
+
+    expect(getThemeSpy).toHaveBeenCalled();
+    expect(res).not.toBeDefined();
+  });
+
+  it('should set bright chart theme', () => {
+    const testTheme = 'deeppurple-amber';
+
+    service.setChartTheme(testTheme);
+  });
+
+  it('should set dark chart theme', () => {
+    const testTheme = 'pink-bluegrey';
+
+    service.setChartTheme(testTheme);
   });
 });
--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
-import { THEMES } from '../constants/themes.constants';
+import { STATE_THEME_KEY } from '../constants/states.constants';
+import { DEFAULT_THEME, THEMES } from '../constants/themes.constants';
+import { LocalStorageService } from './local-storage.service';
 import { StyleManagerService } from './style-manager.service';
+import * as am4core from '@amcharts/amcharts4/core';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+import am4themes_dark from '@amcharts/amcharts4/themes/dark';
 
 export interface IThemeOption {
   backgroundColor: string;
@@ -15,13 +20,58 @@ export interface IThemeOption {
   providedIn: 'root',
 })
 export class ThemeService {
-  constructor(private readonly styleManagerService: StyleManagerService) {}
+  constructor(
+    private readonly styleManagerService: StyleManagerService,
+    private readonly storageService: LocalStorageService
+  ) {}
+
+  private parseThemeFromArray(
+    themeValue: string | null
+  ): IThemeOption | undefined {
+    if (themeValue === null) {
+      return undefined;
+    }
+    return THEMES.find((theme) => {
+      return theme.value === themeValue;
+    });
+  }
+
+  private chooseChartThemeOnBrightness(isDark: boolean | undefined): void {
+    if (isDark !== undefined && !!isDark) {
+      am4core.useTheme(am4themes_dark);
+    } else {
+      am4core.useTheme(am4themes_animated);
+    }
+  }
 
   getThemeOptions(): IThemeOption[] {
     return THEMES;
   }
 
+  getTheme(): string | null {
+    return this.storageService.getItem(STATE_THEME_KEY);
+  }
+
+  getThemeObj(): IThemeOption | undefined {
+    const themeValue = this.getTheme();
+    if (themeValue !== null) {
+      return this.parseThemeFromArray(themeValue);
+    } else {
+      return undefined;
+    }
+  }
+
   setTheme(themeToSet: string): void {
     this.styleManagerService.setStyle('theme', `assets/${themeToSet}.css`);
+  }
+
+  setDefaultTheme(): void {
+    this.styleManagerService.setStyle('theme', `assets/${DEFAULT_THEME}.css`);
+    this.setChartTheme(DEFAULT_THEME);
+  }
+
+  setChartTheme(themeToSet: string | null): void {
+    const isDark = this.parseThemeFromArray(themeToSet)?.isDark;
+    this.chooseChartThemeOnBrightness(isDark);
   }
 }
