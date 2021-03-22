adcfad7a4cd890c3084260e2e5cbeaf8b8bda73c
UsingPython <42588836+UsingPython@users.noreply.github.com>
2021-03-22 20:38:59 +0100

    feat(navbar) Add treemap-links and github icon

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -6,13 +6,33 @@ import { STATE_THEME_KEY } from 'src/app/core/constants/states.constants';
 import { VALID_THEMES_TYPES } from 'src/app/core/constants/themes.constants';
 import { ThemeService } from '../../services/theme.service';
 import { LocalStorageService } from '../../services/local-storage.service';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('indicatorRotate', [
+      state('isTreemapCollapsed', style({ transform: 'rotate(0deg)' })),
+      state('isTreemapExpanded', style({ transform: 'rotate(180deg)' })),
+      transition(
+        'isTreemapExpanded <=> isTreemapCollapsed',
+        animate('225ms cubic-bezier(0.4,0.0,0.2,1)')
+      ),
+    ]),
+  ],
 })
 export class NavbarComponent implements OnInit {
+  isTreemapExpanded = false;
+  faGithub = faGithub;
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -30,6 +50,10 @@ export class NavbarComponent implements OnInit {
     this.chooseTheme();
   }
 
+  goToExternalLink(url: string): void {
+    window.open(url, '_blank', 'noopener');
+  }
+
   themeChangeHandler(themeToSet: string): void {
     this.storageService.setItem(STATE_THEME_KEY, themeToSet);
     this.reloadWindowToApplyThemes(window);
@@ -37,6 +61,10 @@ export class NavbarComponent implements OnInit {
 
   reloadWindowToApplyThemes(window: Window): void {
     window.location.reload();
+  }
+
+  toggleTreemap(): void {
+    this.isTreemapExpanded = !this.isTreemapExpanded;
   }
 
   private chooseTheme(): void {
